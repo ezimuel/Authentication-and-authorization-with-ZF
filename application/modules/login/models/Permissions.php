0b1adf9df3718f8d4ae2e86f3fb22e37f27e5da4
ezimuel <e.zimuel@gmail.com>
2011-04-20 12:12:27 +0200

    Added comments

--- application/modules/login/models/Permissions.php
+++ application/modules/login/models/Permissions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Login_Model_Permissions
  *  
@@ -6,29 +7,37 @@
  */
 class Login_Model_Permissions extends Zend_Db_Table_Abstract
 {
+
     protected $_name = 'permissions';
     protected $_primary = 'id';
-
-    protected $_referenceMap    = array(
+    protected $_referenceMap = array(
         'Role' => array(
-            'columns'           => 'id_role',
-            'refTableClass'     => 'Roles',
-            'refColumns'        => 'id'
+            'columns' => 'id_role',
+            'refTableClass' => 'Roles',
+            'refColumns' => 'id'
         ),
         'Resource' => array(
-            'columns'           => 'id_resource',
-            'refTableClass'     => 'Resources',
-            'refColumns'        => 'id'
-        ));
-        
-	public function getPermissions($role) {
-		$select= $this->getAdapter()->select();
-        $select->from(array('p'=>'permissions'))
-               ->join(array('r'=>'resources'),'r.id=p.id_resource');
+            'columns' => 'id_resource',
+            'refTableClass' => 'Resources',
+            'refColumns' => 'id'
+            ));
+
+    /**
+     * getPermissions
+     * 
+     * @param integer $role
+     * @return array 
+     */
+    public function getPermissions($role)
+    {
+        $select = $this->getAdapter()->select();
+        $select->from(array('p' => 'permissions'))
+                ->join(array('r' => 'resources'), 'r.id=p.id_resource');
         if (!empty($role)) {
-        	$select->where('p.id_role=?',$role);
-        }       
-        $stmt= $this->getAdapter()->query($select);
-		return $stmt->fetchAll();
-	}
+            $select->where('p.id_role=?', $role);
+        }
+        $stmt = $this->getAdapter()->query($select);
+        return $stmt->fetchAll();
+    }
+
 }

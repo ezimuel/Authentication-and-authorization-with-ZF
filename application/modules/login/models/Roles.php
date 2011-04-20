6f09a158fe5ba825c0688285143f8a510ed0c7c0
ezimuel <e.zimuel@gmail.com>
2011-04-20 12:13:04 +0200

    Improved performance of getParentRole

--- application/modules/login/models/Roles.php
+++ application/modules/login/models/Roles.php
@@ -8,20 +8,28 @@ class Login_Model_Roles extends Zend_Db_Table_Abstract
 {
     protected $_name = 'roles';
     protected $_primary = 'id';
-    
     protected $_dependentTables = array('Users','Permissions');
-    
+    /**
+     * getRoles
+     * 
+     * @return object
+     */
     public function getRoles() {
     	return $this->fetchAll(null,'id');
     }
+    /**
+     * getParentRole
+     *
+     * @param integer $role
+     * @return integer|boolean
+     */
     public function getParentRole($role) {
-        $select= $this->select('r2.id as id');
-        $select->from(array('r'=>'roles'))
-               ->join(array('r2'=>'roles'),'r.id_parent=r2.id')
-               ->where('r.id=?',$role);
+        $select= $this->select('id_parent')
+                      ->from(array('r'=>'roles'))
+                      ->where('r.id=?',$role);
         $record= $this->fetchRow($select);
-        if (!empty($record['id'])) {
-            return $record['id'];
+        if (!empty($record['id_parent'])) {
+            return $record['id_parent'];
         }
         return false;
     }
